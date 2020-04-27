ac2f5e26a472d0399701cdefaa4d8ae863d35cb6
Lluminatarion-8431 <captain.a.8431@gmail.com>
2020-04-27 16:25:58 -0700

    Created a Creation test that will check that the size of the stack starts at [0]. Also, created Push and Pop test that will check if the last element is the chosen one and decrease in size. Also created a Error Exceeding Size test, wether for exceeding the size or trying to acces an element when empty.

---
+++ CustomListTests/UnitTest1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sandbox;
+
+namespace CustomListTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Add_AddingOneValueToEmptyCustomList_AddedValueGoesToIndexZero()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd = 10;
+            int expected = 10;
+            int actual;
+
+            // act
+            testList.Add(itemToAdd);
+            actual = testList[0];
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_AddingOneValueToEmptyCustomList_CountOfCustomListIncrements()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd = 10;
+            int expected = 1;
+            int actual;
+
+            // act
+            testList.Add(itemToAdd);
+            actual = testList.Count;
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Creation()
+            //should check the size of the stack is 0 at the beginning
+        {
+            // arrange
+            Stack<int> s = new Stack<int>(3);
+
+            // act
+
+            // assert
+            Assert.AreEqual(0, s.Size);
+        }
+        //should take the last expected element out and decrease the size of the stack
+        [TestMethod]
+        public void Push_Pop()
+        {
+            // arrange
+            Stack<int> s = new Stack<int>(3);
+
+            // act
+            s.Push(1);
+            s.Push(2);
+            s.Push(3);
+
+            // assert
+            int value = s.Pop();
+
+            Assert.AreEqual(3, value);
+            Assert.AreEqual(2, s.Size);
+        }
+
+        // error when trying to access an empty list
+        [TestMethod]
+        public void Too_Much_Pop()
+        {
+            Stack<int> s = new Stack<int>(3);
+            Assert.Throws<ExpenditureProhibitedException>(() => s.Pop());
+        }
+
+        // error when list is full and attempt to add to it
+        [TestMethod]
+        public void Too_much_Push()
+        {
+            // arrange
+            Stack<int> s = new Stack<int>(3);
+
+            // act
+            s.Push(1);
+            s.Push(2);
+            s.Push(3);
+            // assert
+            Assert.Throws<ExceededSizeException>(() => s.Push(4));
+        }
+        // what happens if you add multiple things (or add to a CustomList that already has some values)?
+        // what happens to the last-added item?
+        // what happens to the Count?
+
+        // what happens if you add more items than the initial Capacity of the CustomList?
+
+
+    }
+}
---
+++ Sandbox/CustomList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+    public class CustomList<T>
+    {
+        // member variables (HAS A)
+        private T[] items;
+        List<testList> testLists;
+
+        // constructor (SPAWNER)
+        public CustomList()
+        {
+            items = new T[4];
+            items.add();
+        }
+
+        // member methods (CAN DO)
+        public void Add(T item)
+        {
+            return 10;
+        }
+    }
+}
