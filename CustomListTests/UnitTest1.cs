e3e6e9760dec4b002357e56cb7b2d077d7431517
Lluminatarion-8431 <captain.a.8431@gmail.com>
2020-04-27 20:34:23 -0700

    Recreated UnitTest methods to reflect proper testing for a list method. Added UnitTests that would insert at next index, search for, and remove an item form a list.

--- CustomListTests/UnitTest1.cs
+++ CustomListTests/UnitTest1.cs
@@ -42,59 +42,61 @@ namespace CustomListTests
             Assert.AreEqual(expected, actual);
         }
 
+        
         [TestMethod]
-        public void Creation()
-            //should check the size of the stack is 0 at the beginning
+        public void Add_AddingOneValueToEmptyCustomList_AddedValuesGoToNextIndex()
         {
             // arrange
-            Stack<int> s = new Stack<int>(3);
+            CustomList<int> testList = new CustomList<int>();
+
+            int item = 5;
+            int expected = 5;
+            int actual;
 
             // act
+            testList.Insert(1, item);
+            actual = item[1];
 
             // assert
-            Assert.AreEqual(0, s.Size);
+            Assert.AreEqual(expected, actual);
         }
-        //should take the last expected element out and decrease the size of the stack
+
         [TestMethod]
-        public void Push_Pop()
+        public void Add_AddingOneValueToEmptyCustomList_SearchingAddedValuesInTheIndex()
         {
             // arrange
-            Stack<int> s = new Stack<int>(3);
+            CustomList<int> testList = new CustomList<int>();
+
+            int item = 5;
+            int expected = 5;
+            int actual;
 
             // act
-            s.Push(1);
-            s.Push(2);
-            s.Push(3);
+            testList.Contains(1, item);
+            actual = item[1];
 
             // assert
-            int value = s.Pop();
-
-            Assert.AreEqual(3, value);
-            Assert.AreEqual(2, s.Size);
+            Assert.AreEqual(expected, actual);
         }
 
-        // error when trying to access an empty list
         [TestMethod]
-        public void Too_Much_Pop()
-        {
-            Stack<int> s = new Stack<int>(3);
-            Assert.Throws<ExpenditureProhibitedException>(() => s.Pop());
-        }
-
-        // error when list is full and attempt to add to it
-        [TestMethod]
-        public void Too_much_Push()
+        public void Add_AddingOneValueToEmptyCustomList_RemovingddedValuesInTheIndex()
         {
             // arrange
-            Stack<int> s = new Stack<int>(3);
+            CustomList<int> testList = new CustomList<int>();
+
+            int itemToRemove = 10;
+            int expected = 10;
+            int actual;
 
             // act
-            s.Push(1);
-            s.Push(2);
-            s.Push(3);
+            testList.Remove(0, itemToRemove);
+            actual = itemToRemove[0];
+
             // assert
-            Assert.Throws<ExceededSizeException>(() => s.Push(4));
+            Assert.AreEqual(expected, actual);
         }
+
         // what happens if you add multiple things (or add to a CustomList that already has some values)?
         // what happens to the last-added item?
         // what happens to the Count?
