e4eb9eefb50efde72f4959cc636d396b759426bc
Lluminatarion-8431 <captain.a.8431@gmail.com>
2020-04-28 12:59:04 -0700

    Actually had 5 remove methods the last two test for the proper capacity when removing an item with Item.Capaciy() and the last one checks for the proper count when the last item is removed using Item.Remove().

--- CustomListTests/UnitTest1.cs
+++ CustomListTests/UnitTest1.cs
@@ -99,7 +99,7 @@ namespace CustomListTests
             Assert.AreEqual(expected, actual);
         }
         [TestMethod]
-        public void Remove_RemovingOneValueFromTheCustomList_RemovingaddedValueInTheIndex()
+        public void Remove_RemovingOneValueFromTheCustomList_RemovingAddedValueInTheIndex()
         {
             // arrange
             CustomList<int> testList = new CustomList<int>();
@@ -180,11 +180,7 @@ namespace CustomListTests
             Assert.AreEqual(expected, actual);
 
         }
-        // what happens if you add multiple things (or add to a CustomList that already has some values)?
-        // what happens to the last-added item?
-        // what happens to the Count?
-
-        // what happens if you add more items than the initial Capacity of the CustomList?
+  
         [TestMethod]
         public void Remove_RemovingLastValueFromTheCustomList_CountOfCustomListDecrement()
         {
@@ -207,7 +203,11 @@ namespace CustomListTests
             // assert
             Assert.AreEqual(expected, actual);
         }
+        // what happens if you add multiple things (or add to a CustomList that already has some values)?
+        // what happens to the last-added item?
+        // what happens to the Count?
 
-    }   
+        // what happens if you add more items than the initial Capacity of the CustomList?
+    }
 
 }
